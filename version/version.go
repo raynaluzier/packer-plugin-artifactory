c226ecdf09441598c3d96a7656c7afafd13d1d5d
Rayna Luzier <rluzier@riverpointtechnology.com>
2025-01-09 12:47:26 -0700

    update versions

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "1.0.5"
+	Version           = "1.0.6"
 	VersionPrerelease = "dev"
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
