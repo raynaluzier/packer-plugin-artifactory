8fd97a53a650d91721db76f56e4703d6ea5493f2
Rayna Luzier <rluzier@riverpointtechnology.com>
2025-03-20 07:02:35 -0600

    move code to internal

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"fmt"
 	"os"
 
-	artifactImport "packer-plugin-artifactory/datasource/artifact_import"
-	artifactDownloadOther "packer-plugin-artifactory/datasource/download_other"
-	artifactImage "packer-plugin-artifactory/datasource/source_image"
-	artifactUpload "packer-plugin-artifactory/post-processor/artifact_upload"
-	artifactUpdateProps "packer-plugin-artifactory/post-processor/update_props"
-	artifactUploadOther "packer-plugin-artifactory/post-processor/upload_other"
+	artifactImport "packer-plugin-artifactory/internal/datasource/artifact_import"
+	artifactDownloadOther "packer-plugin-artifactory/internal/datasource/download_other"
+	artifactImage "packer-plugin-artifactory/internal/datasource/source_image"
+	artifactUpload "packer-plugin-artifactory/internal/post-processor/artifact_upload"
+	artifactUpdateProps "packer-plugin-artifactory/internal/post-processor/update_props"
+	artifactUploadOther "packer-plugin-artifactory/internal/post-processor/upload_other"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/hashicorp/packer-plugin-sdk/version"
@@ -20,12 +20,12 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.0.56"
+	Version = "1.1.0"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
